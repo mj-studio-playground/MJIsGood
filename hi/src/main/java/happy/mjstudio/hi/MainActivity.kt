a94151c70a917fa6e1e9ba2fd36834a6850b6fa3
MJ Studio <mym0404@gmail.com>
2020-10-16 17:18:24 +0900

    Fix candidate issue

---
+++ hi/src/main/java/happy/mjstudio/hi/MainActivity.kt
@@ -0,0 +1,11 @@
+package happy.mjstudio.hi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
